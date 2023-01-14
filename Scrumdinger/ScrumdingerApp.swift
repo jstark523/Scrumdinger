0ba8b4ae94728c15f5fe98a30a4acc7df921cd4d
Jason Starkman <jrstarkman@gmail.com>
2023-01-14 16:38:57 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jason Starkman on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
