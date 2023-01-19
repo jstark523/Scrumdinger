7b68cc2269f7508b2780b17a342d8cdcab3462c3
Jason Starkman <jrstarkman@gmail.com>
2023-01-19 13:24:35 -0500

    Created an edit view utilizing states and bindings

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ScrumsView: View {
     let scrums: [DailyScrum]
+    
     var body: some View {
         List{
             ForEach(scrums) { scrum in
@@ -17,16 +18,16 @@ struct ScrumsView: View {
                 }
                 .listRowBackground(scrum.theme.mainColor)
             }
+        }
             .navigationTitle("Daily Scrums")
             .toolbar {
-                        Button(action: {}) {
-                            Image(systemName: "plus")
-                        }
-                    }
-            .accessibilityLabel("New Scrum")
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
         }
     }
-}
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
